ff48737b1bc45ae58ede1af39bef18648f18e61f
Luis Bocaletti <lboca@hotmail.com>
2019-02-12 15:48:35 -0600

    Add project files.

---
+++ CommonServiceLocator.StructureMapAdapter.UnOfficial/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonServiceLocator.StructureMapAdapter.UnOfficial
+{
+    public class Class1
+    {
+    }
+}
